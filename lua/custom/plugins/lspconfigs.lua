4f8df310b3edf561830bc50412dab2d519deecc9
wfhtqp <wfhtqp@gmail.com>
2022-08-29 13:37:58 +0800

    fix: gopls

--- lua/custom/plugins/lspconfigs.lua
+++ lua/custom/plugins/lspconfigs.lua
@@ -22,7 +22,7 @@ local gattach = function(c, b)
   vim.cmd([[
       augroup GO_LSP
         autocmd!
-        autocmd BufWritePre <buffer> lua Goimports(1000)
+        autocmd BufWritePre *.go lua go_org_imports()
         autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil,1000)
       augroup END
     ]])
@@ -61,16 +61,15 @@ lspconfig.gopls.setup {
   }
 }
 
-function Goimports(timeout_ms)
+function go_org_imports(wait_ms)
   local params = vim.lsp.util.make_range_params()
-  params.context = { only = { "source.organizeImports" } }
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-  for _, res in pairs(result or {}) do
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for cid, res in pairs(result or {}) do
     for _, r in pairs(res.result or {}) do
       if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit, "utf-8")
-      else
-        vim.lsp.buf.execute_command(r.command)
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
       end
     end
   end
