4366eff2c256f520afd2431dbc4c97bb8c9ce2e1
wfhtqp <wfhtqp@gmail.com>
2022-10-09 09:42:13 +0800

    feat: disable whichkey map

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -365,21 +365,21 @@ M.nvterm = {
 M.whichkey = {
   plugin = true,
 
-  n = {
-    ["<leader>wK"] = {
-      function()
-        vim.cmd "WhichKey"
-      end,
-      "which-key all keymaps",
-    },
-    ["<leader>wk"] = {
-      function()
-        local input = vim.fn.input "WhichKey: "
-        vim.cmd("WhichKey " .. input)
-      end,
-      "which-key query lookup",
-    },
-  },
+  -- n = {
+  --   ["<leader>wK"] = {
+  --     function()
+  --       vim.cmd "WhichKey"
+  --     end,
+  --     "which-key all keymaps",
+  --   },
+  --   ["<leader>wk"] = {
+  --     function()
+  --       local input = vim.fn.input "WhichKey: "
+  --       vim.cmd("WhichKey " .. input)
+  --     end,
+  --     "which-key query lookup",
+  --   },
+  -- },
 }
 
 M.blankline = {
